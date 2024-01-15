cd7f899a392e647976b21ebf68965738927b803a
dkbnull <dkbnull@163.com>
2024-01-16 01:56:18 +0800

    Date 工具类

--- src/cn/wbnull/helloutil/util/DateUtils.java
+++ src/cn/wbnull/helloutil/util/DateUtils.java
@@ -3,6 +3,10 @@ package cn.wbnull.helloutil.util;
 import cn.wbnull.helloutil.constant.UtilConstants;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Locale;
 
@@ -10,7 +14,7 @@ import java.util.Locale;
  * Date 工具类
  *
  * @author dukunbiao(null)  2018-08-31
- *         https://github.com/dkbnull/Util
+ * https://github.com/dkbnull/HelloUtil
  */
 public class DateUtils {
 
@@ -24,6 +28,22 @@ public class DateUtils {
      */
     public static String dateFormat() {
         return new SimpleDateFormat(UtilConstants.DATE_FORMAT, Locale.CHINA).format(new Date());
+    }
+
+    public static String dateFormat(long timestamp) {
+        return new SimpleDateFormat(UtilConstants.DATE_FORMAT, Locale.CHINA).format(new Date(timestamp));
+    }
+
+    public static String dateFormat(int timestamp) {
+        return new SimpleDateFormat(UtilConstants.DATE_FORMAT, Locale.CHINA).format(new Date(timestamp * 1000L));
+    }
+
+    public static String dateFormat(int timestamp, String format) {
+        return new SimpleDateFormat(format, Locale.CHINA).format(new Date(timestamp * 1000L));
+    }
+
+    public static String dateFormat(LocalDateTime localDateTime) {
+        return localDateTime.format(DateTimeFormatter.ofPattern(UtilConstants.DATE_FORMAT)).replace("T", " ");
     }
 
     /**
@@ -43,6 +63,10 @@ public class DateUtils {
      * @return 时间字符串
      */
     public static String dateFormat(String format) {
+        if (StringUtils.isEmpty(format)) {
+            return null;
+        }
+
         return new SimpleDateFormat(format, Locale.CHINA).format(new Date());
     }
 
@@ -92,6 +116,64 @@ public class DateUtils {
         return dateFormat(date, toFormat);
     }
 
+    public static String dateFormatCurrentTime(String currentTime) {
+        if (StringUtils.isEmpty(currentTime)) {
+            return currentTime;
+        }
+
+        return new SimpleDateFormat(UtilConstants.DATE_FORMAT, Locale.CHINA).format(new Date(Long.parseLong(currentTime) * 1000));
+    }
+
+    public static String dateFormatCurrentTimeMillis(String currentTime) {
+        if (StringUtils.isEmpty(currentTime)) {
+            return currentTime;
+        }
+
+        return new SimpleDateFormat(UtilConstants.DATE_FORMAT, Locale.CHINA).format(new Date(Long.parseLong(currentTime)));
+    }
+
+    public static Date dateParse(String dateValue) throws Exception {
+        if (StringUtils.isEmpty(dateValue)) {
+            return null;
+        }
+
+        return new SimpleDateFormat(UtilConstants.DATE_FORMAT).parse(dateValue);
+    }
+
+    public static Date dateParse(String dateValue, String fromFormat) throws Exception {
+        if (StringUtils.isEmpty(dateValue) || StringUtils.isEmpty(fromFormat)) {
+            return null;
+        }
+
+        return new SimpleDateFormat(fromFormat).parse(dateValue);
+    }
+
+    public static LocalDateTime localDateTimeParse(String dateValue) {
+        if (StringUtils.isEmpty(dateValue)) {
+            return null;
+        }
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern(UtilConstants.DATE_FORMAT);
+        return LocalDateTime.parse(dateValue.replaceAll("T", " "), dtf);
+    }
+
+    public static LocalTime localTimeParse(String dateValue) {
+        if (StringUtils.isEmpty(dateValue)) {
+            return null;
+        }
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
+        return LocalTime.parse(dateValue, dtf);
+    }
+
+    public static Date dateParse(String dateValue, String fromFormat, String toFormat) throws Exception {
+        if (StringUtils.isEmpty(dateValue) || StringUtils.isEmpty(fromFormat) || StringUtils.isEmpty(toFormat)) {
+            return null;
+        }
+
+        return dateParse(dateFormat(dateParse(dateValue, fromFormat), toFormat), toFormat);
+    }
+
     /**
      * 获取Linux时间戳，精确到秒
      *
@@ -99,5 +181,17 @@ public class DateUtils {
      */
     public static String currentTime() {
         return String.valueOf(System.currentTimeMillis() / 1000);
+    }
+
+    public static long toEpochMilli(LocalDateTime localDateTime) {
+        return localDateTime.toInstant(ZoneOffset.of("+8")).toEpochMilli();
+    }
+
+    public static long toEpochTime(LocalDateTime localDateTime) {
+        return localDateTime.toInstant(ZoneOffset.of("+8")).toEpochMilli() / 1000;
+    }
+
+    public static LocalDateTime toLocalDateTime(long timestamp) {
+        return LocalDateTime.ofEpochSecond(timestamp, 0, ZoneOffset.ofHours(8));
     }
 }
